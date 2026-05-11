cf9129c621be9d654380c7f03a5cd8890d644b26
Jayasri <jayasri@gmail.com>
2026-05-11 15:15:55 +0530

    added requirements

--- property/models.py
+++ property/models.py
@@ -76,7 +76,7 @@ class UserRegister(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=255)   # ✅ add this properly
-    phone = models.CharField(max_length=15, blank=True, null=True)
+    role = models.CharField(max_length=50, default='user')  # ✅ இதை add பண்ணு
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
